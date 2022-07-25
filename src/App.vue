e4d853cfd6504a0fcc3927f1b9efccccde2745f0
Mikhail Sitnikov <67879032+justMikhail@users.noreply.github.com>
2022-07-25 18:33:42 +0300

    Test composition API

--- src/App.vue
+++ src/App.vue
@@ -8,13 +8,12 @@
 </template>
 
 <style>
-@import '@/assets/base.css';
+@import "@/assets/base.css";
 
 #app {
   max-width: 1280px;
   margin: 0 auto;
   padding: 2rem;
-
   font-weight: normal;
 }
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,80 +1,62 @@
 <template>
-  <div class="home">
-    
+  <div class='home'>
+    <h2>{{ appTitle }}</h2>
+    <h3>{{ counterData.title }}</h3>
+
     <div>
-      <button @click="decreaseCounter" class="btn">-</button>
-      <span class="counter">{{ counter }}</span>
-      <button @click="increaseCounter" class="btn">+</button>
+      <button @click='decreaseCounter(2)' class='btn'>--</button>
+      <button @click='decreaseCounter(1)' class='btn'>-</button>
+      <span class='counter'>
+        {{ counterData.count }}
+      </span>
+      <button @click='increaseCounter(1)' class='btn'>+</button>
+      <button @click='increaseCounter(2)' class='btn'>++</button>
     </div>
 
+    <p>
+      This counter is {{ oddOrEven }}
+    </p>
+
+    <div class='edit'>
+      <h4>Edit counter title:</h4>
+      <input
+        v-model='counterData.title'
+        type='text'
+      />
+    </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import {reactive, computed} from 'vue';
 
-const counter = ref(0)
+const appTitle = 'Great Counter App';
 
-const increaseCounter = () => {
-  counter.value++
-}
+const counterData = reactive({
+  count: 0,
+  title: 'My Counter'
+});
 
-const decreaseCounter = () => {
-  counter.value--
-}
+const oddOrEven = computed(() => {
+  if (counterData.count % 2 === 0) return 'even';
+  return 'odd';
+});
+
+const increaseCounter = (amount) => {
+  counterData.count += amount;
+};
+
+const decreaseCounter = (amount) => {
+  counterData.count -= amount;
+};
 </script>
-
-<!--
-<script>
-import { ref } from 'vue'
-
-export default {
-  setup() {
-    const counter = ref(0)
-
-    const increaseCounter = () => {
-      counter.value++
-    }
-
-    const decreaseCounter = () => {
-      counter.value--
-    }
-
-    return {
-      counter,
-      increaseCounter,
-      decreaseCounter
-    }
-  }
-}
-</script>
--->
-
-<!--
-<script>
-export default {
-  data() {
-    return {
-      counter: 0
-    }
-  },
-  methods: {
-    increaseCounter() {
-      this.counter++
-    },
-    decreaseCounter() {
-      this.counter--
-    }
-  }
-}
-</script>
--->
 
 <style>
 .home {
   text-align: center;
   padding: 20px;
 }
+
 .btn, .counter {
   font-size: 40px;
   margin: 10px;
